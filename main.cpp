45ea3981e00ffcffebde69c9d9ce024132ead8f0
Mikhailk <mrmops2005@gmail.com>
2021-12-21 11:01:37 +0300

    Added gitignore and vel vxodnie dannie

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
         /**
-        1. Ввести все входные данные
+        1. Ввести все входные данные +
         2. Вывести данные на экран
         3. Вывести  по три числа
         4. Вывод подходящих троек
         5. подсчет подходящих троек
         **/
+    int kol_chisel;
+    cin >> kol_chisel;
+    vector<int> chislo(kol_chisel);
+    for (int i = 0; i < kol_chisel; i++){
+        cin >> chislo[i];
+        }
 
 
 }
