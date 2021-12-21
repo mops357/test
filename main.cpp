3ce3d924f8c2c253a8b6eebacde1bcb2d5c7047b
Mikhailk <mrmops2005@gmail.com>
2021-12-21 11:30:13 +0300

    vivod poxodiashex troek

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@ int main()
         1. Ввести все входные данные +
         2. Вывести данные на экран +
         3. Вывести  по три числа+
-        4. Вывод подходящих троек
-        5. подсчет подходящих троек
+        4. Вывод подходящих троек +
+        5. подсчет подходящей тройки
         **/
     int kol_chisel;
+    int minim = 9999999999999;
+    int vivod;
     cin >> kol_chisel;
     vector<int> chislo(kol_chisel);
     for (int i = 0; i < kol_chisel; i++){
@@ -20,12 +22,16 @@ int main()
     for (int i = 0; i < kol_chisel; i++){
         for (int j = i+1; j < kol_chisel; j++){
             for (int z = j+1; z < kol_chisel; z++){
-                if((chislo[i]!=chislo[j]) && (chislo[i]!=chislo[z])){
-                    cout << chislo[i] << chislo[j] << chislo[z] << " ";
+                if((chislo[i]!=chislo[j]) && (chislo[i]!=chislo[z]) && (chislo[j]!=chislo[z]) ){
+                    if(((chislo[i]+chislo[j]+chislo[z]) % 4 == 0 ) && ((chislo[i]+chislo[j]+chislo[z])<minim)){
+                        minim = (chislo[i]+chislo[j]+chislo[z]);
+                        cout << chislo[i] << chislo[j] << chislo[z] << endl;
+
+                    }
             }
         };
     }
     }
 
-
+    //cout << vivod;
 }
